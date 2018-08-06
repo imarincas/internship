088e7919f606982bbe50f9b40b08cc950970fb39
Erevir <victor.eremia@gmail.com>
2018-08-06 14:55:08 +0300

    new method

--- src/main/java/HipMenuTests/EremiaVictor.java
+++ src/main/java/HipMenuTests/EremiaVictor.java
@@ -1,4 +1,12 @@
 package HipMenuTests;
 
 public class EremiaVictor {
+    String S= "EHHHA";
+    String old="jkghjgh";
+    public  void concat(){
+
+        String word = S+old;
+        System.out.println(word);
+    }
+
 }
