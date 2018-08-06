b7ab4f44c60d6bf43523af71fb1fd9401a902910
iAlexandraAT <alexandra.ilascu@endava.com>
2018-08-06 15:50:55 +0300

    ERROR

--- src/main/java/HipMenuTests/ClasaDeMerge.java
+++ src/main/java/HipMenuTests/ClasaDeMerge.java
@@ -1,4 +1,6 @@
 package HipMenuTests;
 
-public class ClasaDeMerge {
+public class ClasaDeMerge {  public static void main(String[] args) {
+    System.out.println("FATAL ERROR 404 NOT FOUND");
+}
 }
